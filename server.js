28f75f354f2da99e5b6130bfcd0315904b89cef1
son12039 <sonson12039@gmail.com>
2024-09-29 22:05:10 +0900

    0929BasicComent

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: "*",
+});
+
+const msglist = [];
+
+io.on("connection", (socket) => {
+  console.log(socket.id + "들어왔넹");
+  socket.emit("msglist", msglist);
+
+  const clientIp = socket.handshake.address;
+  console.log(`클라이언트 IP: ${clientIp}`);
+
+  socket.on("msg", (msg) => {
+    msglist.push(msg);
+    socket.emit("msg", msg);
+    console.log(msglist);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("서버열렸어");
+});
