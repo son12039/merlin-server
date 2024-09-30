a4a647bcb80ea9003485edbe9e5f0b60cb8092a0
son12039 <sonson12039@gmail.com>
2024-09-30 17:50:12 +0900

    0930

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const io = new Server(server, {
 });
 
 const msglist = [];
+const map = new Map();
+let check = true;
 io.on("connection", (socket) => {
+  if (io.engine.clientsCount > 5) {
+    console.log("5명이 이미 접속 중입니다. 연결을 거부합니다.");
+    socket.emit("connectionError", "현재 게임이 진행 중입니다.");
+    socket.disconnect(); // 연결 종료
+    return;
+  }
   console.log(socket.id + "들어왔넹");
   io.emit("usercount", io.engine.clientsCount);
   socket.emit("msglist", msglist);
@@ -31,6 +39,19 @@ io.on("connection", (socket) => {
     }
 
     io.emit("msglist", msglist);
+  });
+  const characters = ["멀린", "퍼시멀", "시민", "암살자", "모르가나"];
+  socket.on("start", () => {
+    if (check) {
+      check = false;
+      const clients = Array.from(io.sockets.sockets.keys());
+      const shuffledNames = characters.sort(() => Math.random() - 0.5);
+      for (let i = 0; i < 5; i++) {
+        map.set(clients[i], characters[i]);
+      }
+      io.emit("map", Array.from(map.entries()));
+    } else {
+    }
   });
 });
 
