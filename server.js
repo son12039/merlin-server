5fca8f75bf5233f412ab2c5a9e19d8409f25f43b
son12039 <sonson12039@gmail.com>
2024-09-30 12:56:46 +0900

    0930_count

--- server.js
+++ server.js
@@ -9,10 +9,16 @@ const io = new Server(server, {
 });
 
 const msglist = [];
-
 io.on("connection", (socket) => {
   console.log(socket.id + "들어왔넹");
+  io.emit("usercount", io.engine.clientsCount);
   socket.emit("msglist", msglist);
+  // @@ 클라이언트 접속하자마자 실행되어야하는 것들
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + " 나갔넹");
+    io.emit("usercount", io.engine.clientsCount);
+  });
 
   const clientIp = socket.handshake.address;
   console.log(`클라이언트 IP: ${clientIp}`);
